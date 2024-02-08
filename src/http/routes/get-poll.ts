ae93a2b07a275f0886e906b46f3fdef7f9eb10f4
RAFAEL DE SOUSA ALMENDRA <rafaelalmendra28@gmail.com>
2024-02-08 10:29:29 -0300

    feat: get poll

---
+++ src/http/routes/get-poll.ts
@@ -0,0 +1,23 @@
+import z from "zod";
+import { FastifyInstance } from "fastify";
+
+// libs
+import { prisma } from "lib";
+
+export const getPoll = async (app: FastifyInstance) => {
+  app.get("/polls:pollId", async (request, reply) => {
+    const getPollParams = z.object({
+      pollId: z.string().uuid(),
+    });
+
+    const { pollId } = getPollParams.parse(request.params);
+
+    const responsePoll = await prisma.poll.findUnique({
+      where: {
+        id: pollId,
+      },
+    });
+
+    return reply.send({ responsePoll });
+  });
+};
