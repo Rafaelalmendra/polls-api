1e37993a5b18a1a92177fc7f442779d3d0b4c376
RAFAEL DE SOUSA ALMENDRA <rafaelalmendra28@gmail.com>
2024-02-08 12:52:21 -0300

    feat: create vote on poll

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,17 @@
 import fastify from "fastify";
-import { createPoll, getPoll } from "http/routes";
+import cookie from "@fastify/cookie";
+import { createPoll, getPoll, voteOnPoll } from "http/routes";
 
 const app = fastify();
+
+app.register(cookie, {
+  secret: "polls-app-secret-key",
+  hook: "onRequest",
+});
+
 app.register(getPoll);
 app.register(createPoll);
+app.register(voteOnPoll);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server is running on port 3333");
